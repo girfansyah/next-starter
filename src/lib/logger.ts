d2e081f658d3596192afb1286022dcb40f924bbd
Gilang Irfansyah <oadacode@gmail.com>
2024-06-02 17:28:18 +0700

    feat: add logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+import { showLogger } from '../constants/env';
+
+export default function logger(object: unknown, comment?: string): void {
+  if (!showLogger) return;
+
+  console.log(
+    '%c ============== INFO LOG \n',
+    'color: #22D3EE',
+    `${typeof window !== 'undefined' && window?.location.pathname}\n`,
+    `=== ${comment ?? ''}\n`,
+    object,
+  );
+}
