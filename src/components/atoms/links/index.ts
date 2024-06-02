b4aaaafd272621e1b6aad23c8264583af8b39ea1
Gilang Irfansyah <oadacode@gmail.com>
2024-06-02 17:42:40 +0700

    refactor: remove links

--- src/components/atoms/links/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './arrow-link';
-export * from './button-link';
-export * from './icon-link';
-export * from './primary-link';
-export * from './underline-link';
-export * from './unstyled-link';
