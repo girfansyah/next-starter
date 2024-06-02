f511e8f9696e7e01d137bd699abdf228120e1bfc
Gilang Irfansyah <oadacode@gmail.com>
2024-06-02 18:01:23 +0700

    refactor: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   extends: [
     'eslint:recommended',
     'next',
@@ -13,28 +13,27 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    'no-unused-vars': 'off',
-    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'array-callback-return': 'error',
+    semi: ['off', 'never'],
     'react/no-unescaped-entities': 'off',
-
+    'react-hooks/exhaustive-deps': 'off',
+    'react/no-unknown-property': 'off',
+    'no-extra-boolean-cast': 'off',
+    'linebreak-style': ['off', 'unix'],
     'react/display-name': 'off',
     'react/jsx-curly-brace-presence': [
       'warn',
       { props: 'never', children: 'never' },
     ],
 
-    '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'warn',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
     'simple-import-sort/exports': 'warn',
     'simple-import-sort/imports': [
       'warn',
